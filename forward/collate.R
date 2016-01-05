d4c113a4874fdf8e9ede717159f3f15870445934
Earl Bellinger <bellinger@seismo16.mps.mpg.de>
2016-01-05 20:57:34 +0100

    Updated grid collation

--- forward/collate.R
+++ forward/collate.R
@@ -7,35 +7,34 @@ library(lpSolve)
 library(parallel)
 library(parallelMap)
 
+output_fname <- 'grid.dat'
 sim_dir <- 'simulations'
 simulations <- file.path(sim_dir, list.files(sim_dir))
 simulations <- simulations[grep('.dat', simulations)]
 
 # Load data
-load_data <- function(f) {
-    DF <- read.table(f, header=1, check.names=0)
-    DF <- DF[,-which(grepl("mass|Dnu_", names(DF)))]
+load_data <- function(filename, num_points=100) {
+    DF <- read.table(filename, header=1, check.names=0)
+    DF <- DF[, -which(grepl("mass|Dnu_", names(DF)))]
     DF$age <- DF$age - min(DF$age)
     DF <- DF[DF$age <= 13.8,]
     
-    n <- 100
-    if (nrow(DF) < n) return(NULL)
-    N <- length(DF$Hc)
-    ideal <- seq(max(DF$Hc), min(DF$Hc), length=n)
+    nrow.DF <- length(DF$Hc)
+    if (nrow.DF < num_points) return(NULL)
+    ideal <- seq(max(DF$Hc), min(DF$Hc), length=num_points)
     cost.mat  <- outer(ideal, DF$Hc, function(x, y) abs(x-y))
-    row.signs <- rep("==", n)
-    row.rhs   <- rep(1, n)
-    col.signs <- rep("<=", N)
-    col.rhs   <- rep(1, N)
+    row.signs <- rep("==", num_points)
+    row.rhs   <- rep(1, num_points)
+    col.signs <- rep("<=", nrow.DF)
+    col.rhs   <- rep(1, nrow.DF)
     sol <- lp.transport(cost.mat, "min", row.signs, row.rhs,
         col.signs, col.rhs)$solution
     DF[apply(sol, 1, which.max),]
 }
-parallelStartMulticore(max(1, detectCores()/2))
+parallelStartMulticore(max(1, detectCores()))
 seis.DF <- do.call(rbind, parallelMap(load_data, simulations))
-print(nrow(seis.DF))
+print(paste(nrow(seis.DF), "rows"))
 print(sapply(seis.DF, fivenum))
 
 # Save data
-write.table(seis.DF, file.path('grids', 'deleter.dat'), quote=FALSE, 
-    sep='\t', row.names=FALSE)
+write.table(seis.DF, output_fname, quote=FALSE, sep='\t', row.names=FALSE)
